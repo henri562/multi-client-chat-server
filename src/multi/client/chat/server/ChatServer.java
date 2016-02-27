f75b0f85c111fcba048b4b7229ed31dc5d4b88cc
Henry Lin <linm@mail.npu.edu>
2016-02-26 20:26:43 -0800

    first commit

---
+++ src/multi/client/chat/server/ChatServer.java
@@ -0,0 +1,16 @@
+package multi.client.chat.server;
+
+/**
+ *
+ * @author Mengchuan Lin
+ */
+public class ChatServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
